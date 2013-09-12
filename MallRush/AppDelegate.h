7206ce4005f0725962844386510bc549513b735f
ivelinivanov <ivelin.ivanov@mentormate.com>
2013-09-12 12:40:13 +0300

    Initial Commit

---
+++ MallRush/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MallRush
+//
+//  Created by Ivelin Ivanov on 9/12/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MallRush/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MallRush
+//
+//  Created by Ivelin Ivanov on 9/12/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
